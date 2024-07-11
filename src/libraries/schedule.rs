0abbf06dadbeb401108482fe6c9d5ca5a7f6d412
DreamsOneiro <dreams.kong@gmail.com>
2024-07-12 03:04:26 +0800

    Update schedule.rs

--- src/libraries/schedule.rs
+++ src/libraries/schedule.rs
@@ -1,4 +1,4 @@
-use crate::{Races, time};
+use crate::{Races, time, Race};
 
 // Search and return index of current race
 pub fn search_current(races: &Vec<Races>) -> Option<usize> {
@@ -12,3 +12,13 @@ pub fn search_current(races: &Vec<Races>) -> Option<usize> {
     None
 }
 
+// Search and return index of current race
+pub fn find_current(races: &Vec<Race>) -> Option<usize> {
+    let time_now = time::utc_now();
+    for (i, race) in races.iter().enumerate() {
+        if time_now < race.main_race() {
+            return Some(i);
+        }
+    }
+    None
+}
