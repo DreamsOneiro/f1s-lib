cce14ec083d64e4823f5526aba2f8f31cb9f8ace
DreamsOneiro <dreams.kong@gmail.com>
2024-07-23 02:02:23 +0800

    change http client to ureq

--- src/libraries/api.rs
+++ src/libraries/api.rs
@@ -12,31 +12,11 @@ impl API {
     }
 
     pub fn request(mut self) -> Self {
-        self.json = reqwest_url(&self.url);
+        self.json = ureq::get(&self.url)
+        .call()
+        .expect("Problem retrieving data")
+        .into_string()
+        .expect("Problem converting to string");
         return self;
-
-        fn reqwest_url(url: &str) -> String {
-            reqwest::blocking::get(url)
-                .expect(&format!("Problem retreiving data from: {url}, check connection"))
-                .text()
-                .expect(&format!("Problem converting data from: {url} to text"))
-        }
     }
-}
-
-// Depreciated, Ergast will be shutting down end of 2024
-pub fn api_pull(url: &str) -> serde_json::Value {
-    let data = reqwest::blocking::get(url)
-        .expect("Problem retreiving data, check connection")
-        .text()
-        .expect("Problem converting to text");
-    let data: serde_json::Value = serde_json::from_str(&data)
-        .expect("Problem converting data");
-    data.get("MRData")
-        .expect("Code 1: Problem reading data")
-        .get("RaceTable")
-        .expect("Code 2: Problem reading data")
-        .get("Races")
-        .expect("Code 3: Problem reading data")
-        .clone()
 }
